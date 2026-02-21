7407ea04b0740a5c220d61f0fb55642cf401fa73
Lucidreline <casta.ma502@gmail.com>
2026-02-21 10:46:05 -0800

    We can now create and get users.

--- apps/api/src/Routes/users.routes.ts
+++ apps/api/src/Routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Request, Response, Router } from "express";
 import { prisma } from '../lib/prisma.js'
 
+interface CreateUserInput {
+    username: string
+}
+
 const router = Router()
 
+
+// get all users
 router.get('/', async (req: Request, res: Response) => {
     try {
         const allUsers = await prisma.user.findMany()
@@ -10,6 +16,20 @@ router.get('/', async (req: Request, res: Response) => {
     }
     catch (err) {
         res.status(500).json({ error: "Coudn't find Users", err })
+    }
+})
+
+// create new user
+router.post('/', async (req: Request, res: Response) => {
+    const body = req.body as CreateUserInput
+    try {
+        const newUser = await prisma.user.create({
+            data: body
+        })
+        res.json(newUser)
+    }
+    catch (err) {
+        res.status(500).json({ msg: `Couldn't make your boi ${body.username}` })
     }
 })
 
